f123c3d1c805f8b3696104c8210e7a7da88e965e
RAVURU SAI VIKAS <saivikasravuru@gmail.com>
2025-06-27 00:24:28 +0530

    Add files via upload

---
+++ two_sum.py
@@ -0,0 +1,13 @@
+# two_sum.py
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
+
+# Input from user
+nums = list(map(int, input("Enter numbers separated by space: ").split()))
+target = int(input("Enter target sum: "))
+result = two_sum(nums, target)
+print("Indices:", result)
